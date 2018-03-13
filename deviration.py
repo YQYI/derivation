0eb5f787a236b0617bf5b8ebd2bcb356ce49f225
YQYI <YQY@qq.com>
2018-03-13 09:33:56 +0800

    renew

--- deviration.py
+++ deviration.py
@@ -4,69 +4,106 @@ import statistc
 
 #拟合
 def polyfit(x, y, maxDegree):
-    result =[0,-1]
+    """
+
+    :param x:feature
+    :param y: class
+    :param maxDegree: maxDgree
+    :return: args meas function parameters
+    """
+    args=[]
     for i in range(maxDegree+1):
-        args = np.polyfit(x, y, i)
-        r=statistc.r2(args,x,y)
-        AIC=statistc.AIC(args,x,y)
-        BIC=statistc.BIC(args,x,y)
-
-    return result
+        args.append(list(np.polyfit(x, y, i)))
+    return args
 
 
+#find each park's range
+def locate_park(file_name, table_name, ID):
+    """
 
-#extract data
-import xlrd
-data = xlrd.open_workbook('july.xlsx')
-data = data.sheet_by_name('Sheet1')
-nr = data.nrows # 获取总行数
-parkID=[]
-tag=[]
-for i in range(nr):
-    cell_value1 = data.cell_value(i, 0) # 获取第一行第二列单元格的值
-    if cell_value1=="FID_":
-        tag.append(i-1)
-        tag.append(i+1)
-        parkID.append(data.cell_value(i-1, 0))
-del tag[0]
-tag.append(nr)
-#print(tag)
+    :param file_name: xlsx file name
+    :param table_name: sheet name
+    :param ID: such as '_FID_'
+    :return: tag means row range,parkID records park name
+    """
+    import xlrd
+    data = xlrd.open_workbook(file_name).sheet_by_name(table_name)
+    park_name=[]
+    tag=[]
+    for i in range(data.nrows):
+        cell_value1 = data.cell_value(i, 0) # 获取第一行第二列单元格的值
+        if cell_value1==ID:
+            tag.extend([i-1, i+1])
+            park_name.append(data.cell_value(i-1, 0))
+    del tag[0]
+    tag.append(data.nrows)
+    return tag, park_name
 
-#process
-l=len(tag)
-i=0
-j=0
-forWrite=[]
-while(i<=l-2):
-    yRaw=data.col_slice(4,tag[i],tag[i+1])
-    xRaw=data.col_slice(5,tag[i],tag[i+1])
-    y=[cell.value for cell in yRaw]
-    print(y)
-    x=[cell.value for cell in xRaw]
-    print(x)
-    #拟合
-    result=polyfit(x,y,3)
-    #计算峰值
-    sta=statistc.computeLocalMax(result[0])
-    #print(sta)
-    forWrite.append([parkID[j],result,sta])
-    i+=2
-    j+=1
 
-#write
-print(forWrite)
-import xlwt
-f = xlwt.Workbook()
-sheet1 = f.add_sheet(u'sheet1', cell_overwrite_ok=True)  # 创建sheet
-row0 = [u' ', u'polynomial fitting', u'R^2', u'L', u'LCI']
-column0 = [" "]+[i[0] for i in (forWrite)]
-for i in range(0, len(row0)):
-    sheet1.write(0, i, row0[i])
-for i in range(0, len(column0)):
-    sheet1.write(i, 0, column0[i])
-    if i!=0:
-        sheet1.write(i, 1, str(forWrite[i-1][1][0]))
-        sheet1.write(i, 2, str(forWrite[i-1][1][1]))
-        sheet1.write(i, 3, str(forWrite[i-1][2][0]))
-        sheet1.write(i, 4, str(forWrite[i-1][2][1]))
-f.save('julyResult.xlsx')  # 保存文件
+def loop_compute(file_name, table_name, ID):
+    """
+
+    :param file_name: xlsx file name
+    :param table_name: sheet name
+    :return:
+    """
+    import xlrd
+    # file_name='March.xlsx'
+    # table_name='Sheet1'
+    data = xlrd.open_workbook(file_name).sheet_by_name(table_name)
+    tag, park_name = locate_park(file_name, table_name, ID)
+    l=len(tag)//2
+    i=0
+    content=[]
+    while(i<l):
+        yRaw=data.col_slice(4,tag[i*2],tag[i*2+1])
+        xRaw=data.col_slice(5,tag[i*2],tag[i*2+1])
+        y=[cell.value for cell in yRaw]
+        x=[cell.value for cell in xRaw]
+        #拟合
+        result=polyfit(x, y, 6)
+        r = []
+        aic = []
+        bic = []
+        peak_x_y = []
+        for args in result:
+            r.append(statistc.r2(args,x,y))
+            aic.append(statistc.AIC(args, x, y))
+            bic.append(statistc.BIC(args, x, y))
+            peak_x_y.append(statistc.min_local_max(args))
+        #计算峰值
+        #sta=statistc.computeLocalMax(result[0])
+        #print(sta)
+        content.append([park_name[i],result,r,aic,bic,peak_x_y])
+        i+=1
+    return content
+
+
+def write_file(content,file_name):
+    #write
+    import xlwt
+    f = xlwt.Workbook()
+    sheet1 = f.add_sheet(u'sheet1', cell_overwrite_ok=True)  # 创建sheet
+    row0 = [u' ', u'多项式', u'R*2', u'AIC', u'BIC', u'P', u'peakX', u'peakY']
+    #column0 = [i[0] for i in (content)]
+    for i in range(0, len(row0)):
+        sheet1.write(0, i, row0[i])
+    for i in range(0, len(content)):
+        sheet1.write(i*8+1, 0, content[i][0])
+        for j in range(7):
+            sheet1.write(i*8+j+1, 1, str(content[i][1][j]))
+            sheet1.write(i*8+j+1, 2, str(content[i][2][j]))
+            sheet1.write(i * 8 + j + 1, 3, str(content[i][3][j]))
+            sheet1.write(i * 8 + j + 1, 4, str(content[i][4][j]))
+            sheet1.write(i * 8 + j + 1, 6, str(content[i][5][j][0]))
+            sheet1.write(i * 8 + j + 1, 7, str(content[i][5][j][1]))
+    f.save(file_name)  # 保存文件
+
+
+file_name = 'March.xlsx'
+save_file = 'MarchResult1.xlsx'
+table_name = 'Sheet1'
+ID = 'FID_'
+content = loop_compute(file_name, table_name, ID)
+print(content)
+write_file(content, save_file)
--- statistc.py
+++ statistc.py
@@ -1,28 +1,46 @@
 #-*-coding:utf-8-*-
 #AIC=2k - 2ln(L)
 import numpy as np
-def computeLocalMax(args):
-#def compute(args):
+def min_local_max(args):
+    print("************************************")
     ori = np.poly1d(args)
-    der = ori.deriv()
-    root = np.roots(der)
-    validRoot = []
+    print("args:",args)
+    f_deri = ori.deriv()
+    print("一阶倒数:",f_deri)
+    s_deri = f_deri.deriv()
+    print("二阶导数：",s_deri)
+    root = np.roots(f_deri)
+    print("根:",root)
+    #print("root",root)
     for i in root:
-        if isinstance(i, float):
-            validRoot.append(i)
+        if type(i)!=type(np.complex128(1)):
+            print(type(s_deri(i)))
+            if s_deri(i) < 0:
+                print("二阶带入结果:", i, s_deri(i))
+                return [i, ori(i)]
+    return [None,None]
 
-    # print("root:",root)
-    result = ori(validRoot)
-    print("value:", result)
-    result = result.tolist()
-    if result:
-        i = result.index(max(result))
-        return [validRoot[i], result[i]]
-    else:
-        return [-1, -1]
 
 def AIC(args,x,y):
-    pass
+    from math import log
+    """
+    AIC=2k-ln(L)
+    K是模型参数个数,L是模型似然函数
+    :param args:参数个数
+    :param x:
+    :param y:
+    :return:
+    """
+    func = np.poly1d(args)
+    num = len(x)
+    p = len(args)
+    predict_y = func(x)
+    real_y = y
+    res_all = np.sum((predict_y-real_y) ** 2)
+    test = num*log(res_all)
+    #print(test)
+    result = num*log(res_all)+2*(p + 1)-num*log(num)
+    return result
 
 def r2(args,x,y):
     # r-squared
@@ -36,4 +54,13 @@ def r2(args,x,y):
     return rSqure
 
 def BIC(args,x,y):
-    pass
+    from math import log
+    func = np.poly1d(args)
+    num = len(x)
+    p = len(args)
+    predict_y = func(x)
+    real_y = y
+    res_all = np.sum((predict_y - real_y) ** 2)
+    # print(test)
+    result = num * log(res_all) + (p + 1)*log(num) - num * log(num)
+    return result
